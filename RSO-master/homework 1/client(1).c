dd4ab81728d9b58b0be0c9d8450987be93526608
Karolina <karolina.baranowska@ericsson.com>
2019-08-23 09:28:59 +0200

    Add files via upload

---
+++ RSO-master/homework 1/client(1).c
@@ -0,0 +1,321 @@
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <endian.h>   
+#include <byteswap.h>
+#include <pthread.h>
+#include <signal.h>
+#include <errno.h>
+
+#define ROOT_ANSWER_CODE 0x1000001
+#define DATE_ANSER_CODE 0x10002
+#define ROOT_REQUEST_CODE 0x1
+#define DATE_REQUEST_CODE 0x2
+
+void print_menu();
+void perform_square_root_request(int);
+void perform_date_request(int);
+void proceed_root_answer(int);
+void proceed_date_answer(int);
+// function that is run by the thread
+void* incomming_request_handler(void*);
+
+int read_wrapper(int, void*, size_t);
+
+int write_wrapper(int, void*, size_t);
+
+// this variables are modified only by one thread
+// that's why there are thread-safe
+int root_request_counter = 0;
+int date_request_counter = 0;
+int should_run = 1;
+int sockfd;
+
+// https://developer.ibm.com/tutorials/l-sockpit/
+int main () {
+   int request_no = 0;
+   
+   socklen_t len;
+   struct sockaddr_in address;
+   int result;
+
+   sockfd = socket (AF_INET, SOCK_STREAM, 0);
+   if(sockfd == -1) {
+	perror(strerror(errno));	
+	exit(1);
+   }	
+
+   address.sin_family = AF_INET;
+   address.sin_addr.s_addr = inet_addr ("127.0.0.1");
+   address.sin_port = htons (9734);
+   len = sizeof (address);
+
+   result = connect (sockfd, (struct sockaddr *) &address, len);
+   if (result == -1) {
+       perror (strerror(errno));
+       exit (1);
+   }
+
+	// run incomming request hanlder
+	pthread_t thread_id;
+	int error = pthread_create(&thread_id, NULL, incomming_request_handler, NULL);
+	if(error != 0) {
+		perror("\nThread can;t be created!\n");
+		exit(1);
+	}
+
+	while(should_run) {
+		do {	    
+		    print_menu();
+		    printf("Your option: ");
+     	            scanf("%d", &request_no);
+
+			switch(request_no) {
+			  case 1:
+				perform_square_root_request(sockfd);
+				break;
+			  case 2:
+				perform_date_request(sockfd);
+				break;
+			  case 3:
+				should_run = 0;
+			     	break;
+			}
+		} while(request_no < 1 && request_no > 3);
+		
+	}
+   //pthread_join(thread_id, NULL);	
+   //int returned_value = pthread_kill(thread_id, SIGKILL);
+   //if(returned_value > 0) printf("Error closing the thread!");
+   int returned_value = pthread_cancel(thread_id);
+  // pthread_setcancelstate
+   if(returned_value > 0) fprintf(stderr, "Error closing the thread!");
+   close (sockfd);
+   printf("\nBYE!\n");
+   exit (0);
+}
+// https://www.ibm.com/support/knowledgecenter/en/SSLTBW_2.3.0/com.ibm.zos.v2r3.bpxbd00/rtrea.htm
+int read_wrapper(int socketfd, void* buffer, size_t size){
+	size_t total_read = 0, total_left = size;
+	char* buffer_pointer = (char*)buffer;
+
+	while(total_left > 0) {
+		ssize_t current_read = read(socketfd, buffer_pointer, total_left); 
+		
+		if(current_read <= 0){
+			// end of the file reached
+			// peer called close() function			
+			if(current_read == 0) {
+				perror("End of the file reached!");
+				close(socketfd);
+				return 0;
+				//exit(1);
+				
+			} else if(current_read < 0) {
+				// EINTR the call was interrupted before any data was read
+				if(errno == EINTR) continue;
+				perror(strerror(errno));
+				close(socketfd);
+				return -1;
+				//exit(1);			
+			}
+
+		} else {
+			total_read += current_read;
+			total_left -= current_read;
+			buffer_pointer += current_read;
+		}
+	}
+	
+	
+	return total_read;
+}
+
+int write_wrapper(int socketfd, void* buffer, size_t size) {
+	size_t total_written = 0, total_left = size; 
+	char* buffer_pointer = (char*)buffer;
+	
+	while(total_left > 0) {
+		ssize_t current_written = write(socketfd, buffer_pointer, total_left);
+		
+ 
+		if(current_written <= 0) {
+
+			if(current_written == 0) {
+				perror("End of the file reached!");
+				close(socketfd);
+				exit(1);
+			} else if(current_written < 0) {
+				perror(strerror(errno));
+				close(socketfd);
+				exit(1);			
+			}
+		} else { 
+			total_written += current_written;
+			total_left -= current_written; 
+			buffer_pointer += current_written;		
+		}
+		
+	}
+
+	return total_written;
+}
+
+// pthread_cleanup_push to do the necessary cleanups - in our
+// case there is no cleanupss
+void* incomming_request_handler(void* vargp) {
+	//printf("Thread receiving responses started!");
+	while(should_run) {
+		uint32_t h_answer_code, h_request_id;
+		uint64_t answer_code_request_id;
+        	
+		read_wrapper(sockfd, &answer_code_request_id, sizeof(uint64_t));
+		
+		memcpy(&h_answer_code, &answer_code_request_id, sizeof(uint32_t));
+		memcpy(&h_request_id, (char*)&answer_code_request_id + sizeof(uint32_t), sizeof(uint32_t));
+	
+	
+		// decode the reuqest_id
+
+		h_answer_code = ntohl(h_answer_code);
+		h_request_id = ntohl(h_request_id);
+
+
+		//printf("\nThe answer code is: %x\n", h_answer_code);
+	
+		switch(h_answer_code) {
+			case ROOT_ANSWER_CODE:
+				// root answer 
+				if(root_request_counter < h_request_id) {
+					perror("Wrong request id!");
+					exit(1);
+				}
+				proceed_root_answer(sockfd);			
+				break;
+			case DATE_ANSER_CODE:
+				// date answer 
+				if(date_request_counter < h_request_id) {
+					perror("Wrong request id!");
+					exit(1);
+				}
+				proceed_date_answer(sockfd);
+				break;
+			default:
+				perror("Wrong answer code!");
+				exit(1);
+		}
+
+	}
+}
+
+
+void print_menu() {
+	printf("Select option\n");
+	printf("1 - square root\n");
+	printf("2 - current date\n");
+	printf("3 - close connection\n"); 
+}
+
+
+void proceed_date_answer(int sockfd) { 
+	char request_coded[8];
+
+	uint32_t req_code;
+	uint32_t req_id;
+	uint32_t data_length;
+
+	read_wrapper(sockfd, &data_length, sizeof(uint32_t));
+	
+	uint32_t h_data_length = ntohl(data_length);		
+	
+	char* date = malloc((h_data_length * sizeof(char)) + 1);
+	if(date == NULL) {
+		perror("Error allocating memory!");
+		exit(1);
+	}
+
+	 	
+	read_wrapper(sockfd, date, h_data_length * sizeof(char));
+
+	*(date + h_data_length) = '\0';
+	
+	printf("\nDate received: %s\n", date);
+
+	free(date);
+
+}
+
+void proceed_root_answer(int sockfd) {
+	char request_coded[8];
+
+
+	memset(request_coded, '\0', 8 * sizeof(char));
+	read_wrapper(sockfd, request_coded, 8 * sizeof(char));
+
+	uint64_t result;
+	memcpy(&result, request_coded, sizeof(result));
+	uint64_t h_result = be64toh(result);
+
+	double d_result;
+	memcpy(&d_result, &h_result, sizeof(d_result));
+	printf("\n The result is: %lf\n", d_result);
+}
+
+void perform_square_root_request(int sockfd) {
+	double my_digit = 0;
+	printf("Please insert your digit:");
+	scanf("%lf", &my_digit);
+	uint64_t my_digit_bits;
+	memcpy(&my_digit_bits, &my_digit, sizeof(my_digit));
+
+
+	uint64_t n_my_digit_bits = htobe64(my_digit_bits);
+
+
+	// encode root request
+	uint32_t request_code = ROOT_REQUEST_CODE;
+	uint32_t request_id = root_request_counter;
+	char request_coded[16];
+	// code for network transfer
+	uint32_t n_request_code = htonl(request_code);
+	uint32_t n_request_id = htonl(request_id);
+
+	memcpy(request_coded, &n_request_code, sizeof(n_request_code));
+	memcpy(request_coded + sizeof(n_request_code), &n_request_id , sizeof(n_request_id));
+	memcpy(request_coded + (sizeof(n_request_code) + sizeof(n_request_id)), &n_my_digit_bits, sizeof(n_my_digit_bits));
+
+	write_wrapper(sockfd, request_coded, 16 * sizeof(char));
+	root_request_counter++;
+}
+
+
+
+void perform_date_request(int sockfd) {
+	uint32_t request_code = DATE_REQUEST_CODE;
+	uint32_t request_id = date_request_counter;
+	char request_coded[8];
+	// code for network transfer
+	uint32_t n_request_code = htonl(request_code);
+	uint32_t n_request_id = htonl(request_id);
+
+	memcpy(request_coded, &n_request_code, sizeof(n_request_code));
+	memcpy(request_coded + sizeof(n_request_code), &n_request_id , sizeof(n_request_id));
+
+	write_wrapper(sockfd, request_coded, 8 * sizeof(char));
+
+
+	date_request_counter++;
+}	
+
+
+
+
+
+
+
+
---
+++ RSO-master/homework 1/server.c
@@ -0,0 +1,266 @@
+#include <string.h> /* memcpy */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h> /* close */
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <signal.h>
+#include <math.h>
+#include <endian.h>    // __BYTE_ORDER __LITTLE_ENDIAN
+#include <byteswap.h>  // bswap_64()
+#include <time.h>
+#include <locale.h>
+#include <errno.h>
+
+#define BUFFER_SIZE 1024
+#define REQ_LENGTH 8
+#define ROOT_ANSWER_CODE 0x1000001
+#define DATE_ANSER_CODE 0x10002
+#define ROOT_REQUEST_CODE 0x1
+#define DATE_REQUEST_CODE 0x2
+
+void square_root_handler(int, uint32_t);
+
+void root_square_computer(int, uint64_t*, size_t );
+
+void date_handler(int, uint32_t);
+
+int read_wrapper(int, void*, size_t);
+
+int write_wrapper(int, void*, size_t);
+
+int main () {
+   // ignore child signals
+   signal (SIGCHLD, SIG_IGN);
+   int server_sockfd, client_sockfd;
+   socklen_t server_len, client_len;
+   struct sockaddr_in server_address;
+   struct sockaddr_in client_address;
+
+   server_sockfd = socket (AF_INET, SOCK_STREAM, 0);
+   if(server_sockfd == -1) {
+	perror(strerror(errno));
+	exit(1);
+   }	
+
+   server_address.sin_family = AF_INET;
+   // htonl(INADDR_LOOPBACK);
+   //htonl(INADDR_ANY);
+   server_address.sin_addr.s_addr = htonl(INADDR_ANY);
+   server_address.sin_port = htons (9734);
+   server_len = sizeof (server_address);
+
+   int bind_returned_value = bind (server_sockfd, (struct sockaddr *) &server_address, server_len);
+
+	if(bind_returned_value == -1) {
+		perror(strerror(errno));		
+		exit(1);
+	}
+
+   /*  Create a connection queue, ignore child exit details and wait for clients.  */
+
+   int listen_returned_value = listen (server_sockfd, 5);
+   if(listen_returned_value == -1) {
+   	perror(strerror(errno));
+	exit(1);
+    }
+   // signal (SIGCHLD, SIG_IGN);
+
+   while (1) {
+
+       printf ("Server waiting for next connection!\n");
+
+       /*  Accept connection.  */
+
+       client_len = sizeof (client_address);
+       client_sockfd = accept (server_sockfd,
+               (struct sockaddr *) &client_address,
+               &client_len);
+	// error handling
+	if(client_sockfd == -1) {
+		perror(strerror(errno));
+	}
+
+	printf("New connection!\n");
+
+       /*  Fork to create a process for this client and perform a test to see
+           whether we're the parent or the child.  */
+
+       if (fork () == 0) {
+		while(1) {
+		   // the children
+		  uint64_t selector_req_id_buffer;
+		  read_wrapper(client_sockfd, &selector_req_id_buffer, sizeof(uint64_t));
+	          uint32_t selector_l, req_id; 
+		  // copy the memory from the buffer
+		
+		   memcpy(&selector_l, &selector_req_id_buffer, sizeof(uint32_t));
+		   memcpy(&req_id, &selector_req_id_buffer + sizeof(uint32_t), sizeof(uint32_t));		
+		   selector_l = ntohl(selector_l);
+
+		   switch(selector_l) {
+//255
+		       case ROOT_REQUEST_CODE:
+			    // process the square roto request
+		       	    square_root_handler(client_sockfd, req_id);
+			    break;
+			case DATE_REQUEST_CODE:
+			    //printf("Received the date request!\n");
+			    date_handler(client_sockfd, req_id);
+			    break;
+		       default:
+		           printf("Wrong request\n");
+		           exit(1);
+		   }
+		}
+       } else {
+           close (client_sockfd);
+       }
+   }
+}
+
+// https://www.ibm.com/support/knowledgecenter/en/SSLTBW_2.3.0/com.ibm.zos.v2r3.bpxbd00/rtrea.htm
+int read_wrapper(int socketfd, void* buffer, size_t size){
+	size_t total_read = 0, total_left = size;
+	char* buffer_pointer = (char*)buffer;
+
+	while(total_left > 0) {
+		size_t current_read = read(socketfd, buffer_pointer, total_left); 
+		
+		if(current_read <= 0){
+			// end of the file reached
+			// peer called close() function			
+			if(current_read == 0) {
+				perror("End of the file reached!");
+				close(socketfd);
+				exit(1);
+			} else if(current_read < 0) {
+				perror(strerror(errno));
+				close(socketfd);
+				exit(1);			
+			}
+
+		} else {
+			total_read += current_read;
+			total_left -= current_read;
+			buffer_pointer += current_read;
+		}
+	}
+	
+	
+	return total_read;
+}
+
+int write_wrapper(int socketfd, void* buffer, size_t size) {
+	size_t total_written = 0, total_left = size; 
+	char* buffer_pointer = (char*)buffer;
+	
+	while(total_left > 0) {
+		size_t current_written = write(socketfd, buffer_pointer, total_left);
+		
+ 
+		if(current_written <= 0) {
+
+			if(current_written == 0) {
+				perror("End of the file reached!");
+				close(socketfd);
+				exit(1);
+			} else if(current_written < 0) {
+				perror(strerror(errno));
+				close(socketfd);
+				exit(1);			
+			}
+		} else { 
+			total_written += current_written;
+			total_left -= current_written; 
+			buffer_pointer += current_written;		
+		}
+		
+	}
+
+	return total_written;
+}
+
+void square_root_handler(int client_sockfd, uint32_t req_id) {
+	    // process the root request
+	    uint64_t result;
+	    root_square_computer(client_sockfd, &result, sizeof(uint64_t));
+	    char result_coded[16];
+            // code the result
+	    uint32_t root_code_result = htonl(ROOT_ANSWER_CODE);
+	   // printf("\n%x\n", root_code_result);
+	
+
+            memcpy(result_coded, &root_code_result, sizeof(uint32_t));
+	    memcpy(result_coded + sizeof(uint32_t), &req_id, sizeof(uint32_t));
+	    memcpy(result_coded + (2 * sizeof(uint32_t)), &result, sizeof(uint64_t)); 
+	    write_wrapper(client_sockfd, result_coded, 16 * sizeof(char));	
+}
+
+
+
+void root_square_computer(int client_socket, uint64_t* result, size_t size) {
+
+   // read the double value from socket connection
+   uint64_t number;
+   read_wrapper(client_socket, &number, sizeof(uint64_t));	
+
+   uint64_t value_for_host = be64toh(number);
+   double value = 0.0;
+   memcpy(&value, &value_for_host, sizeof(value_for_host));
+
+   double d_result = sqrt(value);
+
+   uint64_t output_value;
+   memcpy(&output_value, &d_result, sizeof(uint64_t));
+
+
+   uint64_t network_result = htobe64(output_value);
+   memcpy(result, &network_result, size);
+
+}
+
+
+void date_handler(int client_sockfd, uint32_t req_id) {
+	
+
+    time_t sekund;
+    struct tm *wsk_strukt;
+    char napis[60];
+
+    setlocale (LC_ALL, "pl_PL");  
+
+    time (&sekund);
+    wsk_strukt = localtime (&sekund);
+    strftime (napis, 60, "%c", wsk_strukt);
+
+
+    uint32_t length_date = strlen(napis);
+    // encode the response
+    char* response_encoded = malloc(12+length_date);
+    if(response_encoded == NULL) {
+    	printf("Error allocating memory!");
+	exit(1); 
+    }
+    // request code
+    uint32_t response_code = DATE_ANSER_CODE;
+    uint32_t n_response_code = htonl(response_code);
+    memcpy(response_encoded, &n_response_code, sizeof(uint32_t));
+	
+    // code req_id	
+    memcpy(response_encoded + sizeof(uint32_t), &req_id, sizeof(uint32_t)); 
+
+    // code date length
+    uint32_t date_length = strlen(napis);
+    uint32_t n_date_length = htonl(date_length);
+    memcpy(response_encoded + (2 * sizeof(uint32_t)), &n_date_length, sizeof(uint32_t)); 
+
+    // code the date
+    // https://stackoverflow.com/questions/1568057/ascii-strings-and-endianness 
+    // string are not affected by endianess
+    memcpy(response_encoded + (3 * sizeof(uint32_t)), napis, strlen(napis));	
+    write_wrapper(client_sockfd, response_encoded, ((12 + length_date) * sizeof(char)));	
+
+    free(response_encoded);
+}
+
